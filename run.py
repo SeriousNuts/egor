75c4d6164f81f6026308a476528e6817b815a13e
Serio <SeriousNuts@yandex.ru>
2023-04-27 21:47:52 +0700

    запуск теперь из run.py

--- run.py
+++ run.py
@@ -1,9 +1,3 @@
 #!flask/bin/python
-from app import app, db
-from app.models import Question, ThreatSource, ObjectOfInfluence, Threat, Option, SessionQuest, OptionConf, Result
-
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'Question': Question, 'ThreatSource': ThreatSource, 'ObjectOfInfluence': ObjectOfInfluence, 'Threat': Threat, 'Option': Option,
-            'SessionQuest': SessionQuest, 'OptionConf': OptionConf, 'Result': Result}
+from app import app
+app.run(debug = True, host='194.58.123.188', port=4500)
